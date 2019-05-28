28fd1eb07ed70bc034c28365ce4a702794837252
Nkazimulo <nkazimuna@gmail.com>
2019-05-28 11:28:22 -0700

    Initial commit

---
+++ app/src/main/java/com/example/phrases/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.phrases;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void buttonTapped(View view){
+
+        int id =  view.getId();
+        String ourId = "";
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId,"raw","com.example.phrases");
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this,resourceId);
+        mediaPlayer.start();
+
+        Log.i("Button tapped ", ourId);
+    }
+
+}
